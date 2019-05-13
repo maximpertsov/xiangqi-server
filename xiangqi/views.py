79711447f2d8f7b718e7fc31626d98bd0f1de9ce
maximpertsov <maxim.pertsov@gmail.com>
2019-05-13 19:44:42 -0400

    Example apis (#1)
    * Add example apis
    
    * Fix bugs
    
    * Move api data to fixtures
    
    * allow-x-origin on init position

--- xiangqi/views.py
+++ xiangqi/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import json
+import os
 
-# Create your views here.
+from django.conf import settings
+from django.http import JsonResponse
+
+
+def load_fixture(fixture):
+    fixture_path = os.path.join(settings.BASE_DIR, 'xiangqi/fixtures', fixture)
+    with open(fixture_path) as f:
+        return json.load(f)
+
+
+def allow_cross_origin(f):
+    def wrapped(*args, **kwargs):
+        response = f(*args, **kwargs)
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+        response["Access-Control-Max-Age"] = "1000"
+        response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+        return response
+    return wrapped
+
+
+@allow_cross_origin
+def get_game(request, pk):
+    data = load_fixture('api__game__{}.json'.format(pk))
+    return JsonResponse(data, status=200)
+
+
+@allow_cross_origin
+def get_initial_position(request):
+    data = load_fixture('api__initial_position.json')
+    return JsonResponse(data, status=200)
