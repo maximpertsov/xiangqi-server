b0a6dcac0e2bab40790bd3589a2f800993be7d85
maximpertsov <maxim.pertsov@gmail.com>
2019-05-23 22:23:39 -0400

    Add some legal move facilities and APIs (#2)
    * Basic move functions
    
    * WIP: legal moves for rooks
    
    * WIP: test pawns instead
    
    * Shorthand fen and pawn legal move test
    
    * Update positions api
    
    * Reduce position api payload
    
    * Update API to serialize in FEN
    
    * Fix cannon placement

--- lib/fen.py
+++ lib/fen.py
@@ -15,14 +15,35 @@ def decode_fen_row(i, row):
     return result
 
 
+# slots instead of ranks and files
+def decode_fen_row2(row):
+    result = []
+    j = 0
+    for ch in row:
+        if ch.isdigit():
+            k = int(ch)
+            result.extend([None] * k)
+            j += k
+            continue
+        result.append(ch)
+        j += 1
+    return result
+
+
 def decode_fen(fen):
     fen_tokens = fen.split(' ')[0].split('/')
     result = list(chain.from_iterable(starmap(decode_fen_row, enumerate(fen_tokens))))
     return result
 
 
+def decode_fen2(fen):
+    fen_tokens = fen.split(' ')[0].split('/')
+    result = list(chain.from_iterable(map(decode_fen_row2, fen_tokens)))
+    return result
+
+
 if __name__ == '__main__':
-    fen = argv[1]
-    pieces = decode_fen(fen)
+    fen = (argv[1])
+    pieces = decode_fen2(fen)
     data = {'pieces': pieces}
     print(json.dumps(data))
