9707e1aa0c97ef8ff3e4683df995d4f6f527b9d5
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-06-09 13:53:31 -0400

    Test GET game response data

--- xiangqi/tests/test_views.py
+++ xiangqi/tests/test_views.py
@@ -9,9 +9,18 @@ register(factories.GameFactory)
 
 
 @pytest.mark.django_db
-def test_get_game(client, game):
+def test_get_game_404(client):
     r = client.get('/api/game/0')
     assert r.status_code == 404
 
+
+@pytest.mark.django_db
+def test_get_game_200(client, game):
     r = client.get('/api/game/{}'.format(game.pk))
     assert r.status_code == 200
+
+    data = r.json()
+    assert data['fen'] == '9/9/9/9/9/9/9/9/9/9'
+    assert data['ranks'] == 10
+    assert data['files'] == 9
+    assert data['players'] == []
