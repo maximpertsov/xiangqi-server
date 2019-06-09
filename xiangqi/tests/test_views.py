396e12a0bf7a3aaaac227085c2b22ee606566392
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-06-09 13:48:45 -0400

    Basic GET game smoke test

--- xiangqi/tests/test_views.py
+++ xiangqi/tests/test_views.py
@@ -1,7 +1,17 @@
 import pytest
+from pytest_factoryboy import register
+
+from xiangqi.tests import factories
+
+register(factories.UserFactory)
+register(factories.PlayerFactory)
+register(factories.GameFactory)
 
 
 @pytest.mark.django_db
-def test_get_game(client):
-    r = client.get('/api/game/2')
+def test_get_game(client, game):
+    r = client.get('/api/game/0')
     assert r.status_code == 404
+
+    r = client.get('/api/game/{}'.format(game.pk))
+    assert r.status_code == 200
