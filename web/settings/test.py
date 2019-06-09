aafbb9fa783e79f503b99b6fc5ce279c22c546aa
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-06-09 13:58:07 -0400

    Testing secret key

--- web/settings/test.py
+++ web/settings/test.py
@@ -1,5 +1,8 @@
 from web.settings.base import *
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'TEST'
+
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
