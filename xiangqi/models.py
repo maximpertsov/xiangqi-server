b12fffd2b097b85412e00cd7e85670872126d19c
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-06-22 13:24:32 -0400

    Get position by natural key

--- xiangqi/models.py
+++ xiangqi/models.py
@@ -23,8 +23,18 @@ class Result(models.Model):
     description = models.TextField()
 
 
+class PositionManager(models.Manager):
+    def get_by_natural_key(self, rank, file):
+        return self.get(rank=rank, file=file)
+
+
 # TODO: make this immutable
 class Position(models.Model):
+    objects = PositionManager()
+
+    class Meta:
+        unique_together = [("rank", "file")]
+
     rank = models.PositiveIntegerField(db_index=True)
     file = models.PositiveIntegerField(db_index=True)
 
