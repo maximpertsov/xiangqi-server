82914e26d92d7dc03250651b053e6141b99d19a4
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-06-23 02:51:12 -0400

    Remove participant role

--- xiangqi/models/participant.py
+++ xiangqi/models/participant.py
@@ -3,9 +3,23 @@ from django.db import models
 from xiangqi.models import Color
 
 
+class ParticipantManager(models.Manager):
+    def get_by_natural_key(self, game, color):
+        return self.get(game=game, color=color)
+
+
 class Participant(models.Model):
+    objects = ParticipantManager()
+
+    class Meta:
+        unique_together = [("game", "color")]
+
     player = models.ForeignKey('player', on_delete=models.CASCADE)
     game = models.ForeignKey('game', on_delete=models.CASCADE)
     score = models.DecimalField(max_digits=5, decimal_places=2)
-    role = models.CharField(max_length=32)
     color = models.CharField(max_length=32, choices=Color.choices())
+
+    def natural_key(self):
+        return (self.game, self.color)
+
+    natural_key.dependencies = ['game']
