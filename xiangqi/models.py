eb1937361b8560a174151481b7bc1f32b62fe888
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-06-23 00:30:28 -0400

    Add player save signals

--- xiangqi/models.py
+++ xiangqi/models.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Color(Enum):
@@ -20,7 +22,18 @@ class Player(models.Model):
     def natural_key(self):
         return self.user.username
 
-    natural_key.dependencies = ['auth.user']
+    natural_key.dependencies = [get_user_model()]
+
+
+@receiver(post_save, sender=get_user_model())
+def create_player(sender, instance, created, **kwargs):
+    if created:
+        Player.objects.create(user=instance)
+
+
+@receiver(post_save, sender=get_user_model())
+def save_player(sender, instance, **kwargs):
+    instance.player.save()
 
 
 class Result(models.Model):
