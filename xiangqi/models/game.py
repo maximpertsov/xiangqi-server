2582106cd4b70ddf8c643ede748594fab8f4c814
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-06-23 03:31:25 -0400

    Generate random id for game

--- xiangqi/models/game.py
+++ xiangqi/models/game.py
@@ -1,7 +1,26 @@
 from django.db import models
+from django.utils.crypto import get_random_string
+
+
+class GameManager(models.Manager):
+    def _generate_xid(self):
+        size = 8
+        while True:
+            xid = get_random_string(8)
+            if self.filter(xid=xid).exists():
+                size += 1
+                continue
+            return xid
+
+    def create(self, xid=None, **kwargs):
+        kwargs['xid'] = self._generate_xid() if xid is None else xid
+        return super().create(**kwargs)
 
 
 class Game(models.Model):
+    objects = GameManager()
+
+    xid = models.CharField(max_length=64, unique=True, editable=False)
     start_time = models.DateTimeField(null=True)
     end_time = models.DateTimeField(null=True)
     player_limit = models.PositiveIntegerField(default=2)
