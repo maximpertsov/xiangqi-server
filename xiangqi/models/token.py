dfe9fb6fa05ba6c529ddedc03004ae29d9073839
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-06-27 00:32:15 -0400

    Update token schema

--- xiangqi/models/token.py
+++ xiangqi/models/token.py
@@ -1,24 +1,30 @@
-import jwt
-from django.conf import settings
 from django.db import models
+from django.utils.timezone import datetime, timedelta
+
+from xiangqi.lib import jwt
 
 DEFAULT_TOKEN_LIFE = 3600
 
 
 class TokenManager(models.Manager):
-    def create(self, username, **kwargs):
-        payload = {'username': username}
-        string = jwt.encode(payload, settings.JWT_SECRET, algorithm='HS256')
-        kwargs.update(string=string)
+    def create(self, user, **kwargs):
+        created_on = datetime.now()
+        expires_on = created_on + timedelta(seconds=DEFAULT_TOKEN_LIFE)
+        payload = {'sub': user.username, 'exp': expires_on}
+        token_string = jwt.encode(payload)
+        kwargs.update(string=token_string, created_on=created_on, expires_on=expires_on)
         return super().create(**kwargs)
 
-    def bulk_create(self):
+    def bulk_create(self, *args, **kwargs):
         raise NotImplementedError('Bulk token creation not allowed')
+
+    def save(self, *args, **kwargs):
+        raise NotImplementedError('Token editing not allowed')
 
 
 class Token(models.Model):
+    objects = TokenManager()
+
     string = models.CharField(max_length=255, unique=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-    expires_in = models.PositiveIntegerField(
-        null=True, default=DEFAULT_TOKEN_LIFE, help_text='In seconds'
-    )
+    created_on = models.DateTimeField()
+    expires_on = models.PositiveIntegerField()
