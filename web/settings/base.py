66121c892efdf7360c63720ede619a557ee46568
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-07-03 11:51:16 -0400

    Add static root

--- web/settings/base.py
+++ web/settings/base.py
@@ -102,7 +102,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
