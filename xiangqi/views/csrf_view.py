844be2665982532c990e35530cd32b4336e6c94e
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-07-04 11:55:22 -0400

    Ping endpoint

--- xiangqi/views/csrf_view.py
+++ xiangqi/views/csrf_view.py
@@ -4,3 +4,7 @@ from django.middleware.csrf import get_token
 
 def csrf(request):
     return JsonResponse({'csrf_token': get_token(request)})
+
+
+def ping(request):
+    return JsonResponse({'result': 'ok'})
