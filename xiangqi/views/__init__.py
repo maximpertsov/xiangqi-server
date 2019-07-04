7c7bbc21021fc331e43f1e6152e98ad1bd6ba3c3
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-07-04 09:25:44 -0400

    Add csrf token endpoint

--- xiangqi/views/__init__.py
+++ xiangqi/views/__init__.py
@@ -3,6 +3,7 @@ isort:skip_file
 """
 
 from .authenticate_view import AuthenticateView
+from .csrf_view import csrf
 from .game_mixin import GameMixin
 from .game_view import GameView
 from .game_move_view import GameMoveView
