023d901e88ef94b4c553c77cfd54c1f7b2ecf3b8
Maxim Pertsov <maxim.pertsov@gmail.com>
2019-12-01 02:44:45 -0500

    Authenticate requires token

--- xiangqi/views/authenticate_view.py
+++ xiangqi/views/authenticate_view.py
@@ -5,15 +5,12 @@ from django.conf import settings
 from django.contrib.auth import authenticate
 from django.core.exceptions import ValidationError
 from django.http import JsonResponse
-from django.utils import timezone
-from django.utils.decorators import method_decorator
 from django.views import View
-from django.views.decorators.csrf import ensure_csrf_cookie
 
-from xiangqi.models import AccessToken, RefreshToken, Token, User
+from xiangqi.models import AccessToken, RefreshToken
 
-JWT_COOKIE = "access_token"
-REFRESH_COOKIE = "refresh_token"
+ACCESS_TOKEN_KEY = "access_token"
+REFRESH_TOKEN_KEY = "refresh_token"
 
 
 def create_access_token(user):
@@ -54,21 +51,21 @@ class LoginView(View):
             return JsonResponse({"error": "Authentication failed"}, status=401)
 
         tokens = {
-            JWT_COOKIE: create_access_token(user),
-            REFRESH_COOKIE: create_refresh_token(user),
+            ACCESS_TOKEN_KEY: create_access_token(user).token,
+            REFRESH_TOKEN_KEY: create_refresh_token(user).token,
         }
 
-        response = JsonResponse({}, status=201)
+        response = JsonResponse(tokens, status=201)
         for cookie, token in tokens.items():
             response.set_cookie(
-                cookie, token.token, domain=settings.CLIENT_DOMAIN, httponly=True
+                cookie, token, domain=settings.CLIENT_DOMAIN, httponly=True
             )
         return response
 
 
-# TODO: break this up into a login and authenticate view?
-@method_decorator(ensure_csrf_cookie, name="dispatch")
 class AuthenticateView(View):
+    http_method_names = ["post"]
+
     @property
     def post_schema(self):
         return {
@@ -82,48 +79,18 @@ class AuthenticateView(View):
 
     def post(self, request):
         try:
-            user = self.get_user_from_cookie(request)
-            if user is None:
-                user = self.get_user_from_payload(request)
-                if user is None:
-                    raise ValidationError("Authentication failed")
-
-            token = self.get_new_token(user)
-
-            response = JsonResponse({"access_token": token.string}, status=201)
-            response.set_cookie(
-                JWT_COOKIE,
-                token.string,
-                expires=token.expires_on,
-                domain=settings.CLIENT_DOMAIN,
-                httponly=True,
-            )
-            return response
+            payload = json.loads(request.body.decode())
+            jsonschema.validate(payload, self.post_schema)
         except json.JSONDecodeError:
             return JsonResponse({"error": "Error parsing request"}, status=400)
         except jsonschema.ValidationError as e:
             return JsonResponse({"error": e.message}, status=400)
-        except ValidationError as e:
-            return JsonResponse({"error": e.message}, status=401)
 
-    def get_user_from_cookie(self, request):
-        try:
-            string = request.COOKIES[JWT_COOKIE]
-            token = self.get_and_expire_active_token(string)
-            return token.get_user()
-        except (KeyError, Token.DoesNotExist, User.DoesNotExist):
-            return
+        access_token = request.COOKIES[ACCESS_TOKEN_KEY]
+        refresh_token = request.COOKIES[REFRESH_TOKEN_KEY]
+        user = AccessToken.objects.get(token=access_token).user
+        if user is None:
+            raise ValidationError("Authentication failed")
 
-    def get_user_from_payload(self, request):
-        payload = json.loads(request.body.decode())
-        jsonschema.validate(payload, self.post_schema)
-        return authenticate(**payload)
-
-    def get_and_expire_active_token(self, string):
-        token = Token.objects.get(string=string)
-        if token.expires_on > timezone.now():
-            return token
-        raise ValidationError("Expired token")
-
-    def get_new_token(self, user):
-        return Token.objects.create(user)
+        tokens = {ACCESS_TOKEN_KEY: access_token, REFRESH_TOKEN_KEY: refresh_token}
+        return JsonResponse(tokens, status=201)
