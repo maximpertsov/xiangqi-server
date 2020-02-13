e3700bc5f7085647ccf143fb4acd071bf2171670
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-12 23:39:57 -0500

    Add move name field

--- xiangqi/models/move.py
+++ xiangqi/models/move.py
@@ -5,6 +5,8 @@ class Move(models.Model):
     game = models.ForeignKey('game', on_delete=models.CASCADE)
     participant = models.ForeignKey('participant', on_delete=models.CASCADE)
     order = models.PositiveIntegerField()
+    # TODO: this should not be nullable
+    name = models.CharField(max_length=10, null=True)
     notation = models.CharField(max_length=32, null=True)
     origin = models.ForeignKey(
         'position', related_name='+', null=True, on_delete=models.PROTECT
