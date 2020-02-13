287c7d8c093a2621659114ca4cd8600458b54bf9
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-13 18:43:27 -0500

    Setup failing test for game moves

--- tests/xiangqi/queries/move/test_game_moves.py
+++ tests/xiangqi/queries/move/test_game_moves.py
@@ -3,7 +3,6 @@ from django.core.management import call_command
 from pytest_factoryboy import register
 
 from tests import factories
-from xiangqi.models import Move
 from xiangqi.queries.move import GameMoves
 
 register(factories.UserFactory)
@@ -34,5 +33,4 @@ def game_with_moves(game, participant_factory, move_factory, player_factory):
 # TODO: remove need for existing pieces
 @pytest.mark.django_db
 def test_game_moves(game_with_moves, pieces):
-    assert Move.objects.count() == 0
     assert GameMoves(game_with_moves).result() == [{}]
