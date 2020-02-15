3c4087d2281d491654a5cc66d7007f471d2d40aa
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-15 18:44:08 -0500

    Simplify move model

--- xiangqi/models/move.py
+++ xiangqi/models/move.py
@@ -5,14 +5,4 @@ class Move(models.Model):
     game = models.ForeignKey('game', on_delete=models.CASCADE)
     participant = models.ForeignKey('participant', on_delete=models.CASCADE)
     order = models.PositiveIntegerField()
-    # TODO: this should not be nullable
-    name = models.CharField(max_length=10, null=True)
-    notation = models.CharField(max_length=32, null=True)
-    # TODO: remove field
-    origin = models.ForeignKey(
-        'position', related_name='+', null=True, on_delete=models.PROTECT
-    )
-    # TODO: remove field
-    destination = models.ForeignKey(
-        'position', related_name='+', null=True, on_delete=models.PROTECT
-    )
+    name = models.CharField(max_length=10)
