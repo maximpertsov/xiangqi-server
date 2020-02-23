5329f985bb81969e78d8f763597e7981e52e2a3d
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-23 12:35:57 -0500

    Allow moves in legal moves query

--- xiangqi/queries/move/legal_moves.py
+++ xiangqi/queries/move/legal_moves.py
@@ -2,8 +2,9 @@ import pyffish
 
 
 class LegalMoves:
-    def __init__(self, fen):
+    def __init__(self, fen, moves):
         self._fen = fen
+        self._moves = moves
 
     def result(self):
-        return pyffish.legal_moves("xiangqi", self._fen, [])
+        return pyffish.legal_moves("xiangqi", self._fen, self._moves)
