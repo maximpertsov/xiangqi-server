539bad89b00b865cd5ca384ebd16bc92748d55ca
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-23 13:49:00 -0500

    TDD create move

---
+++ tests/xiangqi/queries/move/test_create_move.py
@@ -0,0 +1,41 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from xiangqi.queries.move.create_move import CreateMove
+
+
+@pytest.fixture
+def fen():
+    return 'FEN 0'
+
+
+@pytest.fixture
+def new_fen():
+    return 'FEN 1'
+
+
+@pytest.fixture
+def move():
+    return 'MOVE'
+
+
+@pytest.fixture
+def new_legal_moves():
+    return []
+
+
+@pytest.fixture
+def new_move(move, new_fen, new_legal_moves):
+    return {'fen': new_fen, 'move': move, 'legal_moves': new_legal_moves}
+
+
+def test_create_move(move, new_fen, new_move):
+    with patch(
+        'pyffish.get_fen', MagicMock(return_value=new_fen)
+    ) as mock_get_fen, patch(
+        'xiangqi.queries.move.legal_moves', MagicMock(return_value=new_legal_moves)
+    ) as mock_query_legal_moves:
+        assert CreateMove(fen=fen, move=move).result() == new_move
+        mock_get_fen.assert_called_once_with('xiangqi', fen, [move])
+        mock_query_legal_moves.assert_called_once_with(fen, [move])
---
+++ xiangqi/queries/move/create_move.py
@@ -0,0 +1,7 @@
+class CreateMove:
+    def __init__(self, fen, move):
+        self._fen = fen
+        self._move = move
+
+    def result(self):
+        return
