da02c62b59c26789d38e971c4fa211b3af97347a
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-28 09:57:35 -0500

    remove pieces from test

--- tests/xiangqi/operations/move/test_persist_move.py
+++ tests/xiangqi/operations/move/test_persist_move.py
@@ -3,11 +3,9 @@ from unittest.mock import patch
 import pytest
 from django.core.cache import cache
 from django.core.exceptions import ValidationError
-from django.core.management import call_command
 from pytest_factoryboy import register
 
 from tests import factories
-# from xiangqi.models import Move
 from xiangqi.operations.move.persist_move import PersistMove
 from xiangqi.queries.move.game_moves import GameMoves
 
@@ -16,13 +14,6 @@ register(factories.PlayerFactory)
 register(factories.GameFactory)
 register(factories.MoveFactory)
 register(factories.ParticipantFactory)
-
-
-# TODO: remove need for existing pieces
-@pytest.fixture
-def pieces(game):
-    call_command('loaddata', 'pieces.json')
-    return game
 
 
 @pytest.fixture
@@ -43,9 +34,8 @@ def payload():
     return {"move": "b10c8"}
 
 
-# TODO: remove need for existing pieces
 @pytest.mark.django_db
-def test_create_move(game_with_players, participant, payload, pieces):
+def test_create_move(game_with_players, participant, payload):
     assert game_with_players.move_set.count() == 0
     payload.update(player=participant.player.user.username)
     with patch.object(cache, 'set') as mock_cache_set, patch.object(
@@ -61,7 +51,7 @@ def test_create_move(game_with_players, participant, payload, pieces):
 
 
 @pytest.mark.django_db
-def test_create_move_non_participant(game_with_players, payload, pieces):
+def test_create_move_non_participant(game_with_players, payload):
     payload.update(player="Not a game player")
     with pytest.raises(ValidationError):
         PersistMove(game_with_players, payload).perform()
