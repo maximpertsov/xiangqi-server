bc906e108f0380e79c7a6ce0c6d13748440244ac
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-02-29 17:18:05 -0500

    factor out json schema

--- xiangqi/views/fen_move_view.py
+++ xiangqi/views/fen_move_view.py
@@ -1,15 +1,12 @@
-import json
-
-import jsonschema
 from django.core.exceptions import ValidationError
 from django.http import JsonResponse
-from django.utils.functional import cached_property
 from django.views import View
 
 from xiangqi.queries.move.serialize_move import SerializeMove
+from xiangqi.views.payload_schema_mixin import PayloadSchemaMixin
 
 
-class FenMoveView(View):
+class FenMoveView(PayloadSchemaMixin, View):
     def post(self, request):
         try:
             return JsonResponse({"move": self._next_move}, status=200)
@@ -22,19 +19,8 @@ class FenMoveView(View):
         move_name = self.payload["move"]
         return SerializeMove(fen=fen, move_name=move_name).result()
 
-    @cached_property
-    def payload(self):
-        try:
-            result = json.loads(self.request.body.decode("utf-8"))
-            jsonschema.validate(result, self.post_schema)
-            return result
-        except json.JSONDecodeError:
-            return ValidationError("Error parsing request")
-        except jsonschema.ValidationError as e:
-            return ValidationError(str(e))
-
     @property
-    def post_schema(self):
+    def payload_schema(self):
         return {
             "properties": {"fen": {"type": "string"}, "move": {"type": "string"}},
             "required": ["fen", "move"],
---
+++ xiangqi/views/payload_schema_mixin.py
@@ -0,0 +1,26 @@
+import json
+from abc import ABC, abstractmethod
+
+import jsonschema
+from django.core.exceptions import ValidationError
+from django.utils.functional import cached_property
+
+# TODO: test this class!
+
+
+class PayloadSchemaMixin(ABC):
+    @cached_property
+    def payload(self):
+        try:
+            result = json.loads(self.request.body.decode("utf-8"))
+            jsonschema.validate(result, self.payload_schema)
+            return result
+        except json.JSONDecodeError:
+            return ValidationError("Error parsing request")
+        except jsonschema.ValidationError as e:
+            return ValidationError(str(e))
+
+    @property
+    @abstractmethod
+    def payload_schema(self):
+        pass
