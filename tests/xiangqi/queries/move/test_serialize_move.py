96fcf92ead3805a83a12639a368ebbb95a0c4454
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-02 15:17:27 -0500

    fix test

--- tests/xiangqi/queries/move/test_serialize_move.py
+++ tests/xiangqi/queries/move/test_serialize_move.py
@@ -14,12 +14,12 @@ register(factories.MoveFactory)
 
 @pytest.fixture
 def fen():
-    return 'FEN 0'
+    return "FEN 0"
 
 
 @pytest.fixture
 def new_fen():
-    return 'FEN 1'
+    return "FEN 1"
 
 
 @pytest.fixture
@@ -30,16 +30,21 @@ def new_legal_moves():
 @pytest.mark.django_db
 def test_serialize_move(mocker, move__name, new_fen, new_legal_moves):
     mock_get_fen = mocker.patch(
-        'pyffish.get_fen', mocker.MagicMock(return_value=new_fen)
+        "pyffish.get_fen", mocker.MagicMock(return_value=new_fen)
+    )
+    mock_gives_check = mocker.patch(
+        "pyffish.gives_check", mocker.MagicMock(return_value=True)
     )
     mock_query_legal_moves = mocker.patch.object(
-        LegalMoves, 'result', return_value=new_legal_moves
+        LegalMoves, "result", return_value=new_legal_moves
     )
 
     assert SerializeMove(fen=fen, move_name=move__name).result() == {
-        'fen': new_fen,
-        'move': move__name,
-        'legal_moves': new_legal_moves,
+        "fen": new_fen,
+        "gives_check": True,
+        "legal_moves": new_legal_moves,
+        "move": move__name,
     }
-    mock_get_fen.assert_called_once_with('xiangqi', fen, [move__name])
+    mock_get_fen.assert_called_once_with("xiangqi", fen, [move__name])
+    mock_gives_check.assert_called_once_with("xiangqi", fen, [move__name])
     mock_query_legal_moves.assert_called_once_with()
