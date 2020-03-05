0d101b0f49e7a929d1cce29acd5708fee21e98be
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-04 23:20:46 -0500

    default move ordering by pk

--- xiangqi/models/move.py
+++ xiangqi/models/move.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Move(models.Model):
-    game = models.ForeignKey('game', on_delete=models.CASCADE)
-    participant = models.ForeignKey('participant', on_delete=models.CASCADE)
+    class Meta:
+        ordering = ["pk"]
+
+    game = models.ForeignKey("game", on_delete=models.CASCADE)
+    participant = models.ForeignKey("participant", on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
