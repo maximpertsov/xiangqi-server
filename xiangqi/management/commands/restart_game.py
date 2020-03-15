ba8b0b38c23f86155d7eb53e2cb7f282cd5e479a
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-15 11:51:19 -0400

    clear cache when restarting game

--- xiangqi/management/commands/restart_game.py
+++ xiangqi/management/commands/restart_game.py
@@ -1,3 +1,4 @@
+from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 from xiangqi.models import Game
@@ -7,12 +8,16 @@ class Command(BaseCommand):
     help = "Restart game"
 
     def add_arguments(self, parser):
-        parser.add_argument('--game', type=str, help='Game slug', required=True)
+        parser.add_argument("--game", type=str, help="Game slug", required=True)
 
     def handle(self, game=None, *args, **kwargs):
         try:
             Game.objects.get(slug=game).move_set.all().delete()
-            self.stdout.write(self.style.SUCCESS('Reset Game {}'.format(game)))
+            call_command("clear_cache")
+            self.write_success("Reset Game {}".format(game))
         except Game.DoesNotExist as e:
             self.stdout.write(e, self.style.ERROR)
             return
+
+    def write_success(self, message):
+        self.stdout.write(self.style.SUCCESS(message))
