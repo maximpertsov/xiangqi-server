9c189fb06c67f16527be2d5aab33a0fc7d82c71e
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-28 18:51:00 -0400

    WIP: update circle postgres config

--- web/settings/test.py
+++ web/settings/test.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 from web.settings.base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -7,15 +9,6 @@ JWT_SECRET = "TEST"
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "circle_test",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-        "USERNAME": "circleci",
-        "PASSWORD": "",
-    }
-}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
 
 CACHES = {"default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}}
