6ce9eb988078d95bd7e636f1d89a8954c0fb9ab7
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-27 23:38:43 -0400

    Remove position model

--- xiangqi/models/__init__.py
+++ xiangqi/models/__init__.py
@@ -5,7 +5,6 @@ isort:skip_file
 from .color import Color
 from .user import User
 from .player import Player
-from .position import Position
 from .game import Game
 from .participant import Participant
 from .move import Move
