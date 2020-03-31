e8b7a8a8e9d051a8693efd30f5c6cfdcd87855c5
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-03-31 09:50:53 -0400

    games from player

--- xiangqi/models/user.py
+++ xiangqi/models/user.py
@@ -15,3 +15,11 @@ class User(get_user_model()):
 
     def natural_key(self):
         return self.username
+
+    @property
+    def games(self):
+        from xiangqi.models import Game
+
+        return Game.objects.filter(red_player=self).union(
+            Game.objects.filter(black_player=self)
+        )
