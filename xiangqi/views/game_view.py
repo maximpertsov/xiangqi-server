142ddac9a56efea5abe24feae6663e3edb166c8d
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-04-05 14:56:08 -0400

    BREAKING: add game moves to endpoint

--- xiangqi/views/game_view.py
+++ xiangqi/views/game_view.py
@@ -3,6 +3,7 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import ensure_csrf_cookie
 
+from xiangqi.queries.game_moves import GameMoves
 from xiangqi.queries.game_players import GamePlayers
 from xiangqi.views.game_mixin import GameMixin
 
@@ -10,9 +11,13 @@ from xiangqi.views.game_mixin import GameMixin
 @method_decorator(ensure_csrf_cookie, name="dispatch")
 class GameView(GameMixin, View):
     def get(self, request, slug):
-        result = {"players": self._players}
+        result = {"players": self._players, "moves": self._game_moves}
         return JsonResponse(result, status=200)
 
     @property
     def _players(self):
         return GamePlayers(game=self.game).result()
+
+    @property
+    def _game_moves(self):
+        return GameMoves(game=self.game).result()
