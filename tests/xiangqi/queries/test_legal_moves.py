d5ed603ec474577696d0ea243cb66ce90ec5882e
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-04-11 15:29:20 -0400

    Legal moves returns fen

--- tests/xiangqi/queries/test_legal_moves.py
+++ tests/xiangqi/queries/test_legal_moves.py
@@ -5,17 +5,26 @@ from xiangqi.queries.legal_moves import LegalMoves
 
 @pytest.fixture
 def fen():
-    return "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1"
+    return "FEN 0"
 
 
 @pytest.fixture
-def legal_moves():
-    return ["e1e2"]
+def new_fen():
+    return "FEN 1"
 
 
-def test_legal_moves(mocker, fen, legal_moves):
-    mock_get_legal_moves = mocker.patch(
-        "xiangqi.lib.pyffish.legal_moves", mocker.MagicMock(return_value=legal_moves)
+@pytest.fixture
+def legal_move():
+    return "e1e2"
+
+
+def test_legal_moves(mocker, fen, new_fen, legal_move):
+    get_legal_moves = mocker.patch(
+        "xiangqi.lib.pyffish.legal_moves", mocker.MagicMock(return_value=[legal_move])
     )
-    assert LegalMoves(fen=fen).result() == legal_moves
-    mock_get_legal_moves.assert_called_once_with(fen, [])
+    get_fen = mocker.patch(
+        "xiangqi.lib.pyffish.get_fen", mocker.MagicMock(return_value=new_fen)
+    )
+    assert LegalMoves(fen=fen).result() == {legal_move: new_fen}
+    get_fen.assert_called_once_with(fen, [legal_move])
+    get_legal_moves.assert_called_once_with(fen, [])
--- xiangqi/queries/legal_moves.py
+++ xiangqi/queries/legal_moves.py
@@ -8,4 +8,8 @@ class LegalMoves:
     _fen = attr.ib()
 
     def result(self):
+        return {move: pyffish.get_fen(self._fen, [move]) for move in self._legal_moves}
+
+    @property
+    def _legal_moves(self):
         return pyffish.legal_moves(self._fen, [])
