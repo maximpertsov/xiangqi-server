f5855e6691c6b4db9efd7ef18bd4dcb4e3ca71b8
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-06 19:08:02 -0400

    configure jwt

--- web/settings/base.py
+++ web/settings/base.py
@@ -127,15 +127,6 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": True,
-    "ALGORITHM": "HS256",
-    # "SIGNING_KEY": None,
-    "VERIFYING_KEY": None,
-    "AUDIENCE": None,
-    "ISSUER": None,
-    "AUTH_HEADER_TYPES": ("Bearer",),
     "USER_ID_FIELD": "username",
     "USER_ID_CLAIM": "sub",
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
 }
