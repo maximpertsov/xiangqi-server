fb6a38d832a63a807936efec18c9763cd16b1429
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-06 22:10:16 -0400

    add auth view tests

---
+++ tests/xiangqi/views/test_authenticate_view.py
@@ -0,0 +1,44 @@
+import json
+from http.cookies import SimpleCookie
+
+from pytest import mark
+
+
+@mark.django_db
+def test_authenticate(client, player):
+    client.cookies = SimpleCookie({"refresh": "REFRESH_TOKEN"})
+    response = client.post(
+        "/api/token/refresh", data=None, content_type="application/json"
+    )
+    assert response.status_code == 201
+    print(response.json())
+    assert "access_token" in response.json()
+
+
+@mark.django_db
+def test_obtain_token(client, player):
+    password = "s0_s0_secure"
+    player.set_password(password)
+    player.save()
+
+    data = {"username": player.username, "password": password}
+
+    response = client.post(
+        "/api/token/obtain", data=json.dumps(data), content_type="application/json"
+    )
+    assert response.status_code == 200
+    assert set(response.data.keys()) == set(["access", "refresh"])
+
+
+@mark.django_db
+def test_obtain_token_failed(client, player):
+    password = "s0_s0_secure"
+    player.set_password(password)
+    player.save()
+
+    data = {"username": player.username, "password": "badpassword"}
+
+    response = client.post(
+        "/api/token/obtain", data=json.dumps(data), content_type="application/json"
+    )
+    assert response.status_code == 401
