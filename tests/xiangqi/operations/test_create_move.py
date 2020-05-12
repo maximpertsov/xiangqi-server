cf49af642c464e6571e5f485d1392b64167fd789
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-12 19:26:00 -0400

    update create move test

--- tests/xiangqi/operations/test_create_move.py
+++ tests/xiangqi/operations/test_create_move.py
@@ -29,12 +29,34 @@ def event(game_with_players, payload, game_event_factory):
     return game_event_factory(game=game_with_players, payload=payload, name="move")
 
 
+@pytest.fixture
+def mock_pyffish(mocker):
+    return mocker.patch.multiple(
+        "xiangqi.lib.pyffish.xiangqi",
+        get_fen=mocker.DEFAULT,
+        gives_check=mocker.DEFAULT,
+        start_fen=mocker.DEFAULT,
+    )
+
+
 @pytest.mark.django_db
-def test_create_move(event):
+def test_create_move(event, mock_pyffish):
     assert event.game.move_set.count() == 0
 
+    mock_pyffish["start_fen"].return_value = "start fen"
+    mock_pyffish["get_fen"].return_value = "next fen"
+    mock_pyffish["gives_check"].return_value = False
+
     CreateMove(event=event).perform()
-    assert event.game.move_set.first().name == "b10c8"
+    mock_pyffish["start_fen"].assert_called_once_with()
+    mock_pyffish["get_fen"].assert_called_once_with("start fen", ["b10c8"])
+    mock_pyffish["gives_check"].assert_called_once_with("next fen", [])
+
+    first_move = event.game.move_set.first()
+    assert first_move.name == "b10c8"
+    assert first_move.fen == "next fen"
+    assert first_move.gives_check is False
+
     assert event.game.move_set.count() == 1
 
 
