623f392e78de10f318b1dbdc26660675ae0ab61c
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-12 23:25:53 -0400

    WIP: update position queries

--- xiangqi/queries/position.py
+++ xiangqi/queries/position.py
@@ -6,11 +6,11 @@ from xiangqi.lib.pyffish import xiangqi
 from xiangqi.queries.legal_moves import LegalMoves
 
 
-class BaseSerializeFen(ABC):
+class BasePosition(ABC):
     def result(self):
         return {
             "fen": self.fen,
-            "gives_check": self.gives_check,
+            "gives_check": self._gives_check,
             "legal_moves": self._legal_moves,
         }
 
@@ -20,33 +20,25 @@ class BaseSerializeFen(ABC):
         pass
 
     @property
-    @abstractmethod
-    def gives_check(self):
-        pass
-
-    @property
     def _legal_moves(self):
         return LegalMoves(fen=self.fen).result()
 
+    @property
+    def _gives_check(self):
+        return xiangqi.gives_check(self.fen, [])
 
-class SerializeFen(BaseSerializeFen):
-    def __init__(self, fen):
-        self._fen = fen
+
+class Position(BasePosition):
+    def __init__(self, *, previous_fen, move_name):
+        self._move_name = move_name
+        self._previous_fen = previous_fen
 
     @cached_property
     def fen(self):
-        return self._fen
-
-    @property
-    def gives_check(self):
-        return xiangqi.gives_check(self._fen, [])
+        return xiangqi.get_fen(self._previous_fen, [self._move_name])
 
 
-class SerializeInitialFen(BaseSerializeFen):
+class StartingPosition(BasePosition):
     @cached_property
     def fen(self):
         return xiangqi.start_fen()
-
-    @property
-    def gives_check(self):
-        return False
