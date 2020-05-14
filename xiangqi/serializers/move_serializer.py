9d0a19cdd92380572a396621cc049238c42eed9c
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-14 08:35:47 -0400

    WIP: serializers

--- xiangqi/serializers/move_serializer.py
+++ xiangqi/serializers/move_serializer.py
@@ -1,17 +1,24 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
+from xiangqi.lib.pyffish import xiangqi
 from xiangqi.models import Move
-
-from .player_serializer import PlayerSerializer
 
 
 class MoveSerializer(ModelSerializer):
+    game = SlugRelatedField(read_only=True, slug_field="slug")
+    player = SlugRelatedField(read_only=True, slug_field="username")
+
     class Meta:
         model = Move
         fields = ["game", "name", "player"]
 
-    player = PlayerSerializer()
-
     def to_representation(self, instance):
-        # TODO: add fen/gives_check/legal_moves data
-        pass
+        result = super().to_representation(instance)
+        if instance.previous_move:
+            args = [instance.previous_move.fen, [instance.name]]
+            result.update(
+                fen=xiangqi.get_fen(*args),
+                gives_check=xiangqi.gives_check(*args),
+                legal_moves=xiangqi.legal_moves(*args),
+            )
+        return result
--- xiangqi/serializers/player_serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-
-class PlayerSerializer(ModelSerializer):
-    class Meta:
-        model = Player
-        fields = ["username"]
-
-
