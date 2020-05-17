c36fcf904248c1645e142a55bd77c2581f9042d1
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-16 22:52:36 -0400

    wrap xiangqi error

--- xiangqi/lib/pyffish.py
+++ xiangqi/lib/pyffish.py
@@ -8,9 +8,27 @@ VARIANT = "xiangqi"
 
 export_functions = ["get_fen", "gives_check", "legal_moves", "start_fen"]
 
+
+class XiangqiError(Exception):
+    """
+    Exception of all Xiangqi game logic errors
+    """
+
+
+def wrap_system_errors(func):
+    def wrapped(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except SystemError as error:
+            raise XiangqiError(str(error.__cause__))
+
+    return wrapped
+
+
 xiangqi = SimpleNamespace(
+    Error=XiangqiError,
     **{
-        key: partial(func, VARIANT)
+        key: wrap_system_errors(partial(func, VARIANT))
         for key, func in getmembers(pyffish)
         if key in export_functions
     }
