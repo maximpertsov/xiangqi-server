15bfec25c4324fd164bdbbd4f8ad726bd31c5cbb
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-18 22:15:38 -0400

    update auth tests

--- tests/xiangqi/views/test_authenticate_view.py
+++ tests/xiangqi/views/test_authenticate_view.py
@@ -4,20 +4,16 @@ from http.cookies import SimpleCookie
 from pytest import mark
 
 
-# TODO: mock successfully refreshed access token
-
 @mark.django_db
-@mark.skip
 def test_refresh_failed(client, player):
-    client.cookies = SimpleCookie({"refresh": "REFRESH_TOKEN"})
+    client.cookies = SimpleCookie({"token": "REFRESH_TOKEN"})
     response = client.post(
         "/api/token/refresh", data=None, content_type="application/json"
     )
-    assert response.status_code == 401
+    assert response.status_code == 400
 
 
 @mark.django_db
-@mark.skip
 def test_obtain_token(client, player):
     password = "pass123"
     player.set_password(password)
@@ -33,7 +29,6 @@ def test_obtain_token(client, player):
 
 
 @mark.django_db
-@mark.skip
 def test_obtain_token_failed(client, player):
     password = "pass123"
     player.set_password(password)
@@ -44,4 +39,4 @@ def test_obtain_token_failed(client, player):
     response = client.post(
         "/api/token/obtain", data=json.dumps(data), content_type="application/json"
     )
-    assert response.status_code == 401
+    assert response.status_code == 400
