731512ecfea6522c085a9e7e4ad6a6a870c401f4
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-20 09:25:05 -0400

    add gives_check and legal_moves to move serializer representation

--- xiangqi/serializers/move_serializer.py
+++ xiangqi/serializers/move_serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
+from lib.pyffish import xiangqi
 from xiangqi.models import Game, Move, Player
+from xiangqi.queries.legal_moves import LegalMoves
 
 
 class MoveSerializer(serializers.ModelSerializer):
@@ -16,6 +18,14 @@ class MoveSerializer(serializers.ModelSerializer):
     #     if data["player"] not in [game.red_player.username, game.black_player.username]:
     #         raise serializers.ValidationError("Move made by non-game player")
     #     return data
+
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+        result.update(
+            gives_check=xiangqi.gives_check(instance.fen, []),
+            legal_moves=LegalMoves(fen=instance.fen).result(),
+        )
+        return result
 
     class Meta:
         model = Move
