05d3cd0dcd4b8d0fdf005260f4d0f52e98cd7632
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-05-20 11:39:45 -0400

    add initial position

--- xiangqi/serializers/game_serializer.py
+++ xiangqi/serializers/game_serializer.py
@@ -1,7 +1,11 @@
+from collections import OrderedDict
+
 from rest_framework import serializers
 
+from lib.pyffish import xiangqi
 from xiangqi.models import Game
 from xiangqi.models.color import Color
+from xiangqi.queries.legal_moves import LegalMoves
 from xiangqi.serializers.move_serializer import MoveSerializer
 from xiangqi.serializers.player_serializer import PlayerSerializer
 
@@ -13,12 +17,26 @@ class GameSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         result = super().to_representation(instance)
+        self._transform_players(result)
+        self._transform_moves(result)
+        return result
+
+    def _transform_players(self, result):
         # TODO: update client expect `.red_player` and `.black_player` keys,
         # instance of a list of players
         result["red_player"].update(color=Color.RED.value)
         result["black_player"].update(color=Color.BLACK.value)
         result["players"] = [result.pop("red_player"), result.pop("black_player")]
-        return result
+
+    def _transform_moves(self, result):
+        # TODO: this should be it's own serializer and collocated with the move serialier
+        start_fen = xiangqi.start_fen()
+        start_position = OrderedDict(
+            fen=start_fen,
+            gives_check=xiangqi.gives_check(start_fen, []),
+            legal_moves=LegalMoves(fen=start_fen),
+        )
+        result["moves"] = [start_position] + result["moves"]
 
     class Meta:
         model = Game
