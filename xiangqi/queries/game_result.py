247e800dc57c664c595836e66eff6bc21f4bee83
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-06-09 23:42:28 -0400

    Game result checks for accepted draw

--- xiangqi/queries/game_result.py
+++ xiangqi/queries/game_result.py
@@ -3,6 +3,7 @@ from django.utils.functional import cached_property
 
 from lib.pyffish import xiangqi
 from xiangqi.models.color import Color
+from xiangqi.models.draw_event import DrawEventTypes
 from xiangqi.models.fen import Fen
 
 
@@ -11,7 +12,7 @@ class GameResult:
     class Error(Exception):
         pass
 
-    _fen = attr.ib()
+    _move = attr.ib()
 
     @property
     def result(self):
@@ -32,16 +33,28 @@ class GameResult:
     def _is_draw(self):
         if self._both_have_insufficient_material:
             return True
+        if self._has_accepted_draw:
+            return True
         return False
+
+    @property
+    def _has_accepted_draw(self):
+        return self._move.game.event_set.filter(
+            name=DrawEventTypes.ACCEPTED_DRAW.value
+        ).exists()
 
     @property
     def _both_have_insufficient_material(self):
         return all(xiangqi.has_insufficient_material(self._fen, []))
 
-    @cached_property
+    @property
     def _has_legal_moves(self):
         return bool(xiangqi.legal_moves(self._fen, []))
 
     @cached_property
     def _color(self):
         return Fen(fen=self._fen).active_color
+
+    @property
+    def _fen(self):
+        return self._move.fen
