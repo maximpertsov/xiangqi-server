53d998d0038c85dc71f3e4ee6d840f41f7c6fd24
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-06-29 19:03:02 -0400

    Join game

--- xiangqi/serializers/game_request_serializer.py
+++ xiangqi/serializers/game_request_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from xiangqi.models import GameRequest, Player
+from xiangqi.serializers.game_serializer import GameSerializer
 
 
 class GameRequestSerializer(serializers.ModelSerializer):
@@ -12,3 +13,19 @@ class GameRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = GameRequest
         fields = ["player1", "player2", "parameters"]
+
+    def update(self, instance, validated_data):
+        updated_instance = super().update(instance, validated_data)
+        if updated_instance.player1 and updated_instance.player2:
+            self._create_game(updated_instance)
+        return updated_instance
+
+    def _create_game(self, instance):
+        game = GameSerializer(
+            data={
+                "player1": instance.player1.username,
+                "player2": instance.player2.username,
+            }
+        )
+        game.is_valid(raise_exception=True)
+        game.save()
