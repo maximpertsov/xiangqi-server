1d11d7fcbdac2a33ee04ecb50a8bb7af97f56328
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-06-29 16:02:29 -0400

    Rename again

--- xiangqi/models/game_request.py
+++ xiangqi/models/game_request.py
@@ -6,6 +6,6 @@ from xiangqi.models import Player
 
 class GameRequest(models.Model):
     # TODO: limit players to 2?
-    player_set = models.ManyToManyField(Player, related_name='game_request_set')
+    players = models.ManyToManyField(Player)
     parameters = JSONField()
     closed_at = models.DateTimeField(null=True)
