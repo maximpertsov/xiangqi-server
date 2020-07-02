e06d20fe9989b2105ef7b66bef9780b9158ce8a9
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-02 07:59:26 -0400

    basic consumer
    consumer tutorial:
    https://channels.readthedocs.io/en/latest/tutorial/part_2.html
    
    testing tutorial:
    https://blog.scottlogic.com/2019/07/23/Testing-WebSockets-for-beginners.html

--- web/routing.py
+++ web/routing.py
@@ -1,7 +1,13 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import xiangqi.routing
 
 application = ProtocolTypeRouter(
     {
-        # Empty for now (http->django views is added by default)
+        # (http->django views is added by default)
+        "websocket": AuthMiddlewareStack(
+            URLRouter(xiangqi.routing.websocket_urlpatterns)
+        )
     }
 )
