0c96ad7a4cbe137087187921f4ebffd26e9307f8
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-03 19:49:47 -0400

    Try to fix 'too many connections' issue
    source: https://stackoverflow.com/questions/51510259/django-heroku-fatal-too-many-connections-for-role

--- web/settings/production.py
+++ web/settings/production.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
+DATABASES = {"default": dj_database_url.config(conn_max_age=None)}
 
 # Cache
 CACHES = {"default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"}}
