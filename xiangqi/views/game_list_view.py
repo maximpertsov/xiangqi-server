af2666604c84b2f19293221ad0fa90d009ed6496
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-07-12 00:03:38 -0400

    WIP: game list serializer

--- xiangqi/views/game_list_view.py
+++ xiangqi/views/game_list_view.py
@@ -3,8 +3,34 @@ from django.utils.decorators import method_decorator
 from django.utils.functional import cached_property
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic.detail import SingleObjectMixin, View
+from rest_framework import serializers
 
-from xiangqi.models import Player
+from xiangqi.models import Game, Player
+from xiangqi.serializers.move_serializer import MoveSerializer
+
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ["slug", "moves"]
+
+    moves = MoveSerializer(source="move_set", many=True, read_only=True)
+
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+        self._get_current_move(result)
+        return result
+
+    def _get_current_move(result):
+        result["move"] = result.pop("moves")[-1]
+
+
+class GameListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ["games"]
+
+    games = GameSerializer(many=True, read_only=True)
 
 
 @method_decorator(ensure_csrf_cookie, name="dispatch")
