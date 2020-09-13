321a29b322865398701befebb480d6704d463883
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-09-13 16:45:35 -0400

    use standard auth views

--- xiangqi/views/__init__.py
+++ xiangqi/views/__init__.py
@@ -2,7 +2,6 @@
 isort:skip_file
 """
 
-from .authenticate_view import RefreshJSONWebTokenFromCookie
 from .game_view import GameView
 from .game_request_view import GameRequestView
 from .game_event_view import GameEventView
